9c1f26646c9b188a4557dc365678245e4fc0e22b
CMaybe <asbsds123@gmail.com>
2025-06-03 07:41:05 +0000

    Impl: Implement operator== of Account class

--- bear_atm/src/account.cpp
+++ bear_atm/src/account.cpp
@@ -1,6 +1,13 @@
 #include "bear_atm/account.hpp"
 
 #include <limits>
+
+BearATM::Account::Account(const std::string& bank_name,
+                          const std::string& user_name,
+                          const std::string& account_number,
+                          uint64_t balance)
+    : bank_name_(bank_name), user_name_(user_name), account_number_(std::stoull(account_number)), balance_(balance) {}
+
 bool BearATM::Account::deposit(const uint64_t& dollars) {
     if (dollars > std::numeric_limits<uint64_t>::max() - balance_) {
         return false;
@@ -18,3 +25,4 @@ bool BearATM::Account::withdraw(const uint64_t& dollars) {
     balance_ -= dollars;
     return true;
 }
+bool BearATM::Account::operator==(const Account& other) const { return account_number_ == other.account_number_; }
